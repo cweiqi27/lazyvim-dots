8df0f774f49cb59624d5e1d1d5e1cddc7ceab505
Chan Wei Qi <chanweiqi27@gmail.com>
2023-10-15 20:18:36 +0800

    initial commit

---
+++ lua/plugins/misc.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "andweeb/presence.nvim",
+    lazy = true,
+    opts = {
+      enable_line_number = true,
+      neovim_image_text = "waltuh",
+      main_image = "file",
+      buttons = false,
+    },
+  },
+}
